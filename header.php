0fbbd12e776498df2248d4c71968cab0e29071ca
FahimFaysalKabir <badhon_121@yahoo.com>
2018-07-23 09:20:12 +0600

    dynamic menu replaced with given menu,css left

--- header.php
+++ header.php
@@ -10,25 +10,4 @@
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/mobile.js"></script>
 </head>
 <body>
-	<div id="header">
-		<a href="index.html" class="logo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="">
-		</a>
-		<ul id="navigation">
-			<li class="selected">
-				<a href="index.html">home</a>
-			</li>
-			<li>
-				<a href="about.html">about</a>
-			</li>
-			<li>
-				<a href="gallery.html">gallery</a>
-			</li>
-			<li>
-				<a href="blog.html">blog</a>
-			</li>
-			<li>
-				<a href="contact.html">contact</a>
-			</li>
-		</ul>
-	</div>
+
--- menu.php
+++ menu.php
@@ -1,21 +1,21 @@
 <div id="header">
-
+  
+  						
   <?php if ( function_exists( 'get_option_tree') ) : if( get_option_tree( 'logo_uploader') ) : ?>
   					<a href="<?php bloginfo('home'); ?>"><img src="<?php get_option_tree( 'logo_uploader', '', 'true' ); ?>" alt=""/></a>
-  				<?php else : ?>
+  				<?php else : ?>  
               <a href="<?php bloginfo('home'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg"/></a>
   			<?php endif; endif; ?>
 
-
-
-
-  						<?php wp_nav_menu(array(
+         <?php wp_nav_menu(array(
   					 'menu'=>'Primary menu',
   					 'menu_class'=>'',
   					 'menu_id'=>'' ))
   					 ?>
 
+
+
 </div>
-<div id="body">
+
 <!-- slide show coding start from here ,
 i cut out the slide image code to .txt file in desktop!-->
